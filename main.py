2be6c9c8d65145369c6d43f9b52e0c564a458092
maglovskiNenad <maglovskin@gmail.com>
2025-01-02 00:50:44 +0100

    end

--- main.py
+++ main.py
@@ -1,17 +1,37 @@
 import requests
+import time
 
-NAME = "maglovskiNenad"
-URL = f"https://api.github.com/users/{NAME}/events"
 
-req = requests.get(URL)
+def show_activity(user):
+    URL = f"https://api.github.com/users/{user}/events"
 
-data = req.json()
+    req = requests.get(URL)
 
-for name in data:
+    data = req.json()
 
-    commits = f"https://api.github.com/repos/"
-    f"{NAME}/{name["repo"]["name"]}/commits?author={NAME}"
+    for name in data:
+        push = name["type"]
+        name_repo = name["repo"]["name"]
+        user = name["actor"]["login"]
+        match push:
+            case "PushEvent":
+                print(f"- Pushed {len(name["payload"]["commits"])}"
+                      f" commits to {name_repo}.\n")
+            case "WatchEvent":
+                print(f"- Started wathcing {name_repo}\n")
+            case "PullRequestEvent":
+                print(f"- Has {name["payload"]["number"]} pull request\n")
+            case "CreateEvent":
+                print(f"- {name["payload"]["description"]}\n")
 
-a = requests.get(commits)
-d = a.json()
-print(d)
+
+while True:
+    answer = input()
+    user_input = answer.split(" ")
+    if user_input[0] == "github-activity":
+        show_activity(user_input[1])
+        time.sleep(30)
+    elif answer == "q":
+        break
+    else:
+        print("Error,try command 'github-activity' <username> ")
